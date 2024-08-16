902c86f2682ed760c2f4effae712003ddc05a4ea
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-08-16 19:42:50 +0300

    add precommit & biome

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          my Starter kit
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			my Starter kit
+			<p>Test precommit</p>
+		</main>
+	);
 }
